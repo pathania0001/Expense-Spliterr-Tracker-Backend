97e9e8cda1c28628faa2f5256b4caa825e5496dc
pathania0001 <83996978+pathania0001@users.noreply.github.com>
2025-08-22 11:00:43 +0530

    remaining part of above apis

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,11 +1,17 @@
 const {Router} = require('express');
 const userRoutes = require('./user.route');
 const exprenseRoutes = require('./expense.route');
+const groupRoutes = require('./group.route');
+const settlementRoutes = require('./settlement.route');
 
 const v1Routes = Router();
 
 v1Routes.use('/users',userRoutes);
 
 v1Routes.use('/expense',exprenseRoutes);
+
+v1Routes.use('/group',groupRoutes)
+
+v1Routes.use('/settlement',settlementRoutes);
 
 module.exports = v1Routes;
